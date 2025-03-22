a69df0faf864fa0fed9c016923dab33a15d2ae1d
258030 <258030@usn.no>
2025-03-22 22:17:56 +0100

    feature: Now the correlation actually makes some more sense. Possiblity to filter by country

--- CorrelationData.R
+++ CorrelationData.R
@@ -4,6 +4,7 @@ library(reshape2)
 # Script for checking the correlation between data
 # Testing different data
 
+{
 un <- read.csv("un_clean_2018.csv", row.names = 1)
 mp <- read.csv("Figure.S6a_2018.csv", row.names = 1)
 
@@ -11,26 +12,55 @@ source("../CleaningTwoSets.R")
 data <- eqClean(mp, un)
 
 unDf <- data$unDf
-mpdf <- data$mpDf
+mpDf <- data$mpDf
+mpDf <- as.data.frame(t(mpDf))
 
-{
-  unDf <- as.data.frame(t(unDf))
-  unDf$'Location code' <- NULL
-  unDf$`SDMX code**`<- NULL
-  unDf$`Parent code`<- NULL
-  unDf$Year <- NULL
-  mpDf <- as.data.frame(t(mpDf))
+source("../ContinentVector.R")
+
+trashRemover <- function(df) {
+  df <- as.data.frame(t(df))
+  df$'Location code' <- NULL
+  df$`SDMX code**`<- NULL
+  df$`Parent code`<- NULL
+  df$Year <- NULL
+  return(df)
 }
-  correl = cor(unDf, mpDf, method = "spearman")
+
+continent <- function(df1, df2, cont) {
+  unCols <- colnames(df1)[colnames(df1) %in% cont]
+  df1 <- df1[, unCols, drop = FALSE]
+  
+  mpCols <- colnames(df2)[colnames(df2) %in% cont]
+  df2 <- df2[, mpCols, drop = FALSE]
+  return(list(df1 = df1, df2 = df2))
+}
+
+mpDf <- as.data.frame(t(mpDf))
+temp <- continent(unDf, mpDf, asia)
+unEf <- temp$df1
+mpEf <- temp$df2
+
+
+print(ncol(unEf))
+print(ncol(mpEf))
+
+unEf <- as.data.frame(t(unEf))
+mpEf <- as.data.frame(t(mpEf))
+trashRemover(unEf)
+
+
+correl = cor(unEf, mpEf, method = "spearman")
+View(correl)
 
 
 {
-  correl[abs(correl) < 0.7] <- NA
+  correl[abs(correl) < 0.5] <- NA
   correl[correl == 1 ] <- NA
   correl <- correl[rowSums(!is.na(correl)) > 0, colSums(!is.na(correl)) > 0]
   }
   
-  View(correl)
+  
+}
 
 correlM <- melt(correl) 
 
