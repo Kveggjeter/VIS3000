7baf7d15602afb79733031440f57241fe91544e1
258030 <258030@usn.no>
2025-03-22 17:29:17 +0100

    feature: Abstracted the cleaning function and combining it with the correlation script. Now with the possibility to show somewhat relevant barchart (not pretty tho)

--- CorrelationData.R
+++ CorrelationData.R
@@ -1,10 +1,43 @@
+library(ggplot2)
+library(reshape2)
 # Author: Eirik
 # Script for checking the correlation between data
 # Will be differently, it doesn't give us anything at the moment
-# setwd("data")
 
-data <- read.csv("new_data.csv", row.names = 1)
+un <- read.csv("un_clean_2018.csv", row.names = 1)
+mp <- read.csv("Figure.S6a_2018.csv", row.names = 1)
 
-correl = cor(t(data))
+source("../CleaningTwoSets.R")
+data <- eqClean(mp, un)
 
-View(correl)
+unDf <- data$unDf
+mpdf <- data$mpDf
+
+{
+  unDf <- as.data.frame(t(unDf))
+  unDf$'Location code' <- NULL
+  unDf$`SDMX code**`<- NULL
+  unDf$`Parent code`<- NULL
+  unDf$Year <- NULL
+  mpDf <- as.data.frame(t(mpDf))
+}
+  correl = cor(unDf, mpDf, method = "spearman")
+
+
+{
+  correl[correl < 0.7] <- NA
+  correl[correl == 1 ] <- NA
+  correl <- correl[rowSums(!is.na(correl)) > 0, colSums(!is.na(correl)) > 0]
+  }
+  
+  View(correl)
+
+correlM <- melt(correl) 
+
+ggplot(correlM, aes(x = Var1, y = value, fill = Var2)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(title = "Bar Chart av Korrelasjoner", x = "Variabler fra unDf", y = "Korrelasjonsverdi", fill = "Variabler fra mpDf")
+
+
